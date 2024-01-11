8967e15faa44ff522451ab0db46eb389c979f1d6
Sergio <sergiolapria1975@gmail.com>
2024-01-11 19:21:00 +0100

    23
    Signed-off-by: Sergio <sergiolapria1975@gmail.com>

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -4,6 +4,10 @@
 
 #define SHT31_ADDRESS 0x44
 //INCLUDE LIBS
+//constructor for SHT31
+SHT31 sht;
+int val1;
+int val2;
 sensor::sensor(int type){
     //1-sht31
     //2-AHT10
@@ -13,8 +17,7 @@ sensor::sensor(int type){
     switch (type)
     {
     case 1/* constant-expression */:
-        //constructor for SHT31
-        SHT31 sht;
+
         /* code */
         sensor::begin_sensor(1);
         break;
@@ -27,8 +30,6 @@ void sensor::begin_sensor(int type){
     switch (type)
     {
     case 1/* constant-expression */:
-        //constructor for SHT31
-        SHT31 sht;
         /* code */
         sensor::begin_sensor(1);
         break;
@@ -37,5 +38,17 @@ void sensor::begin_sensor(int type){
     }
 }
 void sensor::read_sensor(int type){
+    switch (type)
+    {
+    case 1/* constant-expression */:
+        sht.read();
+        val1=sht.getTemperature()*10;
+        val2=sht.getHumidity()*10;
+        return val1;
+        return val2;
+        break;
     
+    default:
+        break;
+    }
 }
